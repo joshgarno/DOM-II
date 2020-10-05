35af342534fa9d93866eeb774ea2ce0042790877
Josh Garno <you@example.com>
2020-10-04 23:45:56 -0400

    MVP

--- js/index.js
+++ js/index.js
@@ -10,7 +10,31 @@ text1.addEventListener("mouseleave", (e) => {
 });
 
 text1.addEventListener("click", (e) => {
-	text1.style.backgroundColor = '#ccd9ff';
+	text1.style.backgroundColor = '#ffffcc';
+});
+
+text1.addEventListener("dblclick", (e) => {
+	e.stopPropagation();
+	text1.style.backgroundColor = 'blue';
+});
+
+document.querySelectorAll('a').forEach(link1 => {
+	link1.addEventListener("mouseenter", (e) => {
+        link1.style.color = 'orange';
+	});
+});
+
+document.querySelectorAll('a').forEach(link1 => {
+	link1.addEventListener("click", (e) => {
+		e.preventDefault();
+		link1.style.color = 'blue';
+	});
+});
+
+document.querySelectorAll('a').forEach(link1 => {
+	link1.addEventListener("mouseleave", (e) => {
+        link1.style.color = 'black';
+	});
 });
 
 document.querySelectorAll('h2').forEach(text2 => {
@@ -49,14 +73,32 @@ document.querySelectorAll('h4').forEach(text4 => {
 	});
 });
 
-document.querySelectorAll('a').forEach(link1 => {
-	link1.addEventListener("mouseenter", (e) => {
-        link1.style.color = 'orange';
-	});
+const body1 = document.querySelector("body");
+
+body1.addEventListener("keydown", (e) => {
+	body1.style.backgroundColor = '#ffffe6';
 });
 
-document.querySelectorAll('a').forEach(link1 => {
-	link1.addEventListener("mouseleave", (e) => {
-        link1.style.color = 'black';
-	});
+body1.addEventListener("keyup", (e) => {
+	body1.style.backgroundColor = 'white';
+});
+
+body1.addEventListener("wheel", (e) => {
+	body1.style.backgroundColor = "gray";
+});
+
+body1.addEventListener("dblclick", (e) => {
+	body1.style.backgroundColor = "skyblue";
+});
+
+body1.addEventListener("mouseover", (e) => {
+	body1.style.backgroundColor = "pink";
+});
+
+body1.addEventListener("mouseout", (e) => {
+	body1.style.backgroundColor = "green";
+});
+
+body1.addEventListener("drag", (e) => {
+	body1.style.backgroundColor = "black";
 });
